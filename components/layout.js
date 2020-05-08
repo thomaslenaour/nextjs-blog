9238f004443f4da5a95c8ef18859b313d6e7f21c
tlnkorr <lenaourthomas@gmail.com>
2020-05-08 12:29:23 +0200

    change site title

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 
-const name = 'Thomas Le Naour'
+const name = 'TLN'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
